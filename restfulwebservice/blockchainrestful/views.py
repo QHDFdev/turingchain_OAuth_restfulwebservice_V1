24769cdb1333975acafcab30d689c51c972e244f
zzy131250 <zzy131250@outlook.com>
2016-08-17 14:42:47 +0800

    test restful

--- restfulwebservice/blockchainrestful/views.py
+++ restfulwebservice/blockchainrestful/views.py
@@ -1,6 +1,6 @@
 from bigchaindb import Bigchain
-from django.contrib.auth.decorators import login_required
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 import json
 import rethinkdb as r
@@ -8,8 +8,9 @@ import rethinkdb as r
 b = Bigchain()
 conn = r.connect(db='bigchain')
 
-@login_required
+
 @api_view(['GET'])
+@permission_classes((IsAuthenticated, ))
 def get_last_block(request, format=None):
     block = r.table('bigchain').max('block_number').run(conn)
     return Response(json.dumps(block))
