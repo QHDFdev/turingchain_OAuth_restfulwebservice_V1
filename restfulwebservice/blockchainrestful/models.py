5a0e291ed807d1e9c05cb2a044676f27ff271467
zzy131250 <zzy131250@outlook.com>
2016-08-17 12:23:50 +0800

    test restful

--- restfulwebservice/blockchainrestful/models.py
+++ restfulwebservice/blockchainrestful/models.py
@@ -1,24 +1,3 @@
 from array import array
 
 from django.db import models
-
-
-class VoteDetail(models.Model):
-    previous_block = models.CharField
-    timestamp = models.CharField
-    voting_for_block = models.CharField
-    invalid_reason = models.CharField
-    is_block_valid = models.BooleanField
-
-
-class Vote(models.Model):
-    signature = models.CharField
-    node_pubkey = models.CharField
-    vote = VoteDetail
-
-
-class BigBlock(models.Model):
-    block_id = models.CharField
-    block_number = models.IntegerField
-    signature = models.CharField
-    votes = array(Vote)
--- restfulwebservice/blockchainrestful/serializers.py
+++ restfulwebservice/blockchainrestful/serializers.py
@@ -1,15 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from blockchainrestful.models import BigBlock
-
-
-class BigBlockSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = BigBlock
-        fields = ('block_id', 'block_number', 'signature', 'votes')
-
 
 class UserSerializer(serializers.ModelSerializer):
 
