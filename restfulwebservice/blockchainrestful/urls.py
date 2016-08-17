31bbacd1db0eee42fe859817cf65ad52b81d848f
zzy131250 <zzy131250@outlook.com>
2016-08-17 15:21:15 +0800

    test restful

--- restfulwebservice/blockchainrestful/urls.py
+++ restfulwebservice/blockchainrestful/urls.py
@@ -5,6 +5,9 @@ from blockchainrestful import views
 
 urlpatterns = [
     url(r'^block/last/$', views.get_last_block),
+    url(r'^block/(?P<id>[0-9a-z]+)/$', views.get_block_by_id),
+    url(r'^block\?height=(?P<height>[0-9a-z]+)/$', views.get_block_by_height),
+    url(r'^transaction/last/$', views.get_last_transaction),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- restfulwebservice/blockchainrestful/views.py
+++ restfulwebservice/blockchainrestful/views.py
@@ -12,5 +12,110 @@ conn = r.connect(db='bigchain')
 @api_view(['GET'])
 @permission_classes((IsAuthenticated, ))
 def get_last_block(request, format=None):
+    """
+    查询最新的区块
+    输入：无
+    输出：区块信息
+    """
     block = r.table('bigchain').max('block_number').run(conn)
     return Response(json.dumps(block))
+
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ))
+def get_block_by_id(request, block_id, format=None):
+    """
+    通过区块id查询区块
+    输入：区块id
+    输出：区块信息
+    """
+    pass
+
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ))
+def get_block_by_height(request, height, format=None):
+    """
+    通过区块高度返回区块
+    输入：区块高度
+    输出：区块信息
+    """
+    pass
+
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ))
+def get_block_by_transaction_id(request, transaction_id, format=None):
+    """
+    通过交易id查询区块
+    输入：交易id
+    输出：包含该条交易的区块信息
+    """
+    pass
+
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ))
+def get_last_transaction(request, format=None):
+    """
+    查询最新的交易
+    输入：无
+    输出：交易信息
+    """
+    pass
+
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ))
+def get_transaction_by_id(request, transaction_id, format=None):
+    """
+    通过交易id查询交易
+    输入：交易id
+    输出：交易信息
+    """
+    pass
+
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ))
+def get_key_pair(request, format=None):
+    """
+    获取秘钥
+    输入：无
+    输出：公钥和私钥
+    """
+    pass
+
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ))
+def trace_transactions(request, public_key, format=None):
+    """
+    溯源
+    输入：物品公钥
+    输出：交易序列
+    """
+    pass
+
+
+@api_view(['POST'])
+@permission_classes((IsAuthenticated, ))
+def create_transaction(request, public_key, who, where, when, thing, format=None):
+    """
+    区块创建Create交易
+    输入：公钥，人员，地点，时间，物品
+    输出：交易id
+    """
+    pass
+
+
+@api_view(['POST'])
+@permission_classes((IsAuthenticated, ))
+def transfer_transaction(request, tx_originator_public_key, tx_recipient_public_key, tx_originator_private_key, who,
+                         where, when, thing, format=None):
+    """
+    创建Transfer交易
+    输入：交易发起方公钥，交易接收方公钥，交易发起方私钥，人员，地点，时间，物品
+    输出：交易id和剩余物品的秘钥
+    """
+    pass
