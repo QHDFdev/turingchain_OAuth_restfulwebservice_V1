82a2e12a675f9feb9217fa75220efdf792aa3398
zzy131250 <zzy131250@outlook.com>
2016-08-17 12:14:55 +0800

    test restful

--- restfulwebservice/blockchainrestful/models.py
+++ restfulwebservice/blockchainrestful/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 
 
+class VoteDetail(models.Model):
+    previous_block = models.CharField
+    timestamp = models.CharField
+    voting_for_block = models.CharField
+    invalid_reason = models.CharField
+    is_block_valid = models.BooleanField
+
+
+class Vote(models.Model):
+    signature = models.CharField
+    node_pubkey = models.CharField
+    vote = models.OneToOneField(VoteDetail)
+
+
 class BigBlock(models.Model):
     block_id = models.CharField
     block_number = models.IntegerField
     signature = models.CharField
+    votes = models.ManyToOneRel(Vote)
