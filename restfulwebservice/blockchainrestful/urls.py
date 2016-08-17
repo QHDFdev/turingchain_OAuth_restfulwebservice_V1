0e6f5af5ba7f1ded53a7a1bb9016d7a9eb2b754c
zzy131250 <zzy131250@outlook.com>
2016-08-17 17:07:40 +0800

    add query function

--- restfulwebservice/blockchainrestful/urls.py
+++ restfulwebservice/blockchainrestful/urls.py
@@ -4,13 +4,13 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from blockchainrestful import views
 
 urlpatterns = [
+    url(r'^block/$', views.get_block),
     url(r'^block/last/$', views.get_last_block),
     url(r'^block/(?P<id>[0-9a-z]+)/$', views.get_block_by_id),
-    url(r'^block/$', views.get_block),
     url(r'^transaction/last/$', views.get_last_transaction),
+    url(r'^transaction/trace/$', views.trace_transaction),
     url(r'^transaction/(?P<id>[0-9a-z]+)/$', views.get_transaction_by_id),
     url(r'^key-pair/$', views.get_key_pair),
-    url(r'^transaction/trace/$', views.trace_transaction),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
