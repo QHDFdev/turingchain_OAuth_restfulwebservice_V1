8ea616935cc0592b96cdaf4954f8d5b994d26a63
zzy131250 <zzy131250@outlook.com>
2016-08-17 17:56:54 +0800

    test write function

--- restfulwebservice/blockchainrestful/views.py
+++ restfulwebservice/blockchainrestful/views.py
@@ -154,7 +154,7 @@ def create_transaction(request, format=None):
     输出：交易id
     """
     data = json.loads(request.body.decode("utf-8"))
-    payload = {key: value for key, value in data.items() if key is not 'public_key'}
+    payload = data.pop('public_key', None)
     print(payload)
     tx = b.create_transaction(b.me, data['public_key'], None, 'CREATE', payload=payload)
     tx_signed = b.sign_transaction(tx, b.me_private)
