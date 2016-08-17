0180e2bfde2414648bf6814bc57679887eed1116
zzy131250 <zzy131250@outlook.com>
2016-08-17 10:37:22 +0800

    test restful

--- restfulwebservice/blockchainrestful/models.py
+++ restfulwebservice/blockchainrestful/models.py
@@ -5,6 +5,3 @@ class BigBlock(models.Model):
     id = models.CharField
     block_number = models.IntegerField
     signature = models.CharField
-
-    class Meta:
-        ordering = ('block_number',)
