d091ed28df8e5156312b48f955bb5916e9f74dcd
zzy131250 <zzy131250@outlook.com>
2016-08-17 11:15:29 +0800

    test restful

--- restfulwebservice/blockchainrestful/models.py
+++ restfulwebservice/blockchainrestful/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class BigBlock(models.Model):
     id = models.CharField
-    block_number = models.IntegerField
+    block_number = models.CharField
     signature = models.CharField
