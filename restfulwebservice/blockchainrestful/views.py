d9858db60d4a4b9a627e771e58df7264c7e02ee1
zzy131250 <zzy131250@outlook.com>
2016-08-17 12:29:39 +0800

    test restful

--- restfulwebservice/blockchainrestful/views.py
+++ restfulwebservice/blockchainrestful/views.py
@@ -1,5 +1,7 @@
 from bigchaindb import Bigchain
+from django.http import HttpResponse
 from rest_framework.decorators import api_view
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 import json
 import rethinkdb as r
@@ -8,7 +10,17 @@ b = Bigchain()
 conn = r.connect(db='bigchain')
 
 
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
 @api_view(['GET'])
 def get_last_block(request, format=None):
     big_block = r.table('bigchain').max('block_number').run(conn)
-    return Response(json.dumps(big_block['votes']))
+    return JSONResponse(json.dumps(big_block['votes']))
