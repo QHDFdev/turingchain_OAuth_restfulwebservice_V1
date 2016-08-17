ba1e0e435c2bf64c81f642da5d62fe88d473f417
zzy131250 <zzy131250@outlook.com>
2016-08-17 11:21:02 +0800

    test restful

--- restfulwebservice/blockchainrestful/models.py
+++ restfulwebservice/blockchainrestful/models.py
@@ -2,6 +2,5 @@ from django.db import models
 
 
 class BigBlock(models.Model):
-    id = models.CharField
-    block_number = models.CharField
+    block_number = models.IntegerField
     signature = models.CharField
--- restfulwebservice/blockchainrestful/serializers.py
+++ restfulwebservice/blockchainrestful/serializers.py
@@ -8,7 +8,7 @@ class BigBlockSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BigBlock
-        fields = ('id', 'block_number', 'signature')
+        fields = ('block_number', 'signature')
 
 
 class UserSerializer(serializers.ModelSerializer):
