d9e54a9b02c122e00354fcfe1141b1a013950275
zzy131250 <zzy131250@outlook.com>
2016-08-17 11:04:58 +0800

    test restful

--- restfulwebservice/blockchainrestful/serializers.py
+++ restfulwebservice/blockchainrestful/serializers.py
@@ -4,15 +4,15 @@ from rest_framework import serializers
 from blockchainrestful.models import BigBlock
 
 
-class BigBlockSerializer(serializers.HyperlinkedModelSerializer):
+class BigBlockSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BigBlock
-        fields = ('url', 'id', 'block_number', 'signature')
+        fields = ('id', 'block_number', 'signature')
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('url', 'username')
+        fields = ('username',)
