197a6efe486994a654d71e7c5819f2113b1619e0
zzy131250 <zzy131250@outlook.com>
2016-08-17 12:23:10 +0800

    test restful

--- restfulwebservice/blockchainrestful/views.py
+++ restfulwebservice/blockchainrestful/views.py
@@ -1,9 +1,8 @@
 from bigchaindb import Bigchain
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import json
 import rethinkdb as r
-
-from blockchainrestful.serializers import BigBlockSerializer
 
 b = Bigchain()
 conn = r.connect(db='bigchain')
@@ -12,6 +11,4 @@ conn = r.connect(db='bigchain')
 @api_view(['GET'])
 def get_last_block(request, format=None):
     big_block = r.table('bigchain').max('block_number').run(conn)
-    big_block['block_id'] = big_block['id']
-    serializer = BigBlockSerializer(big_block)
-    return Response(serializer.data)
+    return Response(json.dumps(big_block))
