80871b8913bd5538ed970987125da9b59d7dfcb7
zzy131250 <zzy131250@outlook.com>
2016-08-18 12:20:10 +0800

    test write function

--- restfulwebservice/blockchainrestful/views.py
+++ restfulwebservice/blockchainrestful/views.py
@@ -201,6 +201,6 @@ def transfer_transaction(request, format=None):
         tx = b.create_transaction(b.me, public_key, None, 'CREATE', payload=data)
         tx_signed_2 = b.sign_transaction(tx, b.me_private)
         b.write_transaction(tx_signed_2)
-        return Response(json.dumps({'txs': [tx_signed['id'], tx_signed_2['id']], 'key_pair': {'public_key': public_key,
+        return Response(json.dumps({'txs': {'transfer_id': tx_signed['id'], 'create_id': tx_signed_2['id']}, 'key_pair': {'public_key': public_key,
                                                                                               'private_key': private_key}}))
     return Response(json.dumps({'transfer tx id': tx_signed['id']}))
